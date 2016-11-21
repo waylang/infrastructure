f5fae55f369407753256e295214f2c72cd588217
Phil Smith <phil.h.smith@gmail.com>
2016-11-21 09:11:35 -0800

    Bumping infrastructure to 1.3.0.
    closes #18

--- spec/travis_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'metatheory::travis' do
-  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
-
-  it 'includes common' do
-    expect(chef_run).to include_recipe('metatheory::common')
-  end
-end

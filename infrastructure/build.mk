b951f4c9d660f29fd16b18937741aba2ab067cbb
Phil Smith <phil@philhsmith.com>
2018-04-05 23:38:48 -0700

    Changes required to support the book repo
    * Being the first occasion to need ./build, integrate it into the build.  Specifically use
      an "order only" constraint (the |) to mkdir if necessary without triggering
      recompilation of build artifacts merely when directory metadata changes.
    
      Additionally, since glob expansion in targets (apparently) consider only files and not
      declared-but-absent targets, pivot away from declaring dependencies against e.g. all
      directly and instead collect them in variables such as ALL.  Then we may declare a mass
      dependency on build without explicit enumeration.
    * Bring in the few packages and boilerplate ignores that the book repo needs

--- infrastructure/build.mk
+++ infrastructure/build.mk
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-test: infrastructure-test
+TEST += infrastructure-test
 
 .PHONY: infrastructure-test
 infrastructure-test:
